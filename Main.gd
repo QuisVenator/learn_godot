db3023f96f88e9fe627061f5b5f3664b55245302
Manuel René Pauls Toews <paulsmrene@live.com>
2024-02-19 22:49:39 -0300

    Chunk generation, slow

--- Main.gd
+++ Main.gd
@@ -5,6 +5,8 @@ var player: CharacterBody2D
 var debugText: RichTextLabel
 var map: TileMap
 var noise: FastNoiseLite
+var activeChunks: Dictionary = {}
+var renderDistance := 3
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -19,12 +21,25 @@ func _process(delta):
 	var player_pos: Vector2i = Vector2i(round(player.position.x / 16), round(player.position.y / 16))
 	
 	var player_chunk = Vector2i(round((player_pos.x - 8) / float(chunk_size.x)), round((player_pos.y - 8) / float(chunk_size.y)))
-	debugText.text = "Player position: " + str(player_pos)
+	debugText.text = "FPS is: " + str(Engine.get_frames_per_second())
+	debugText.text += "\nPlayer position: " + str(player_pos)
 	debugText.text += "\nPlayer chunk: " + str(player_chunk)
 
-	renderChunk(player_chunk)
 
-func renderChunk(chunk: Vector2i):
+	for x_offset in range(-1 * renderDistance, renderDistance+1):
+		for y_offset in range(-1 * renderDistance, renderDistance+1):
+			var visible_chunk = Vector2i(player_chunk.x + x_offset, player_chunk.y + y_offset)
+			if activeChunks.has(visible_chunk) == false:
+				activeChunks[visible_chunk] = null
+
+				for chunk in activeChunks.keys():
+					if abs(chunk.x - player_chunk.x) > renderDistance or abs(chunk.y - player_chunk.y) > renderDistance: 
+						delete_chunk(chunk)
+						activeChunks.erase(chunk)
+				
+				render_chunk(visible_chunk)
+
+func render_chunk(chunk: Vector2i):
 	var grass_tiles = []
 	for x in range(chunk_size.x):
 		for y in range(chunk_size.y):
@@ -32,26 +47,12 @@ func renderChunk(chunk: Vector2i):
 			if noise.get_noise_2d(map_pos.x, map_pos.y) > 0:
 				grass_tiles.append(map_pos)
 			else:
-				# Delete the cell
 				map.set_cell(0,  map_pos, 1, Vector2i(0, 0), 0)
 	
 	map.set_cells_terrain_connect(0, grass_tiles, 0, 0)
 
-func _get_map_size(map: TileMap) -> Vector2:
-	var cells = map.get_used_cells(0)
-	if cells.size() == 0:
-		return Vector2(0, 0)
-	
-	var upper_left := Vector2.ZERO
-	var lower_right := Vector2.ZERO
-	for cell in cells:
-		if upper_left == null:
-			upper_left = cell
-			lower_right = cell
-		else:
-			upper_left.x = min(upper_left.x, cell.x)
-			upper_left.y = min(upper_left.y, cell.y)
-			lower_right.x = max(lower_right.x, cell.x)
-			lower_right.y = max(lower_right.y, cell.y)
-	
-	return lower_right - upper_left + Vector2(1, 1)
+func delete_chunk(chunk: Vector2i):
+	for x in range(chunk_size.x):
+		for y in range(chunk_size.y):
+			var map_pos := Vector2i(chunk.x * chunk_size.x + x, chunk.y * chunk_size.y + y)
+			map.set_cell(0,  map_pos, -1)
