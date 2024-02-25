3ed36b577e8507f76a8c317b831799eac259a4b2
Manuel René Pauls Toews <paulsmrene@live.com>
2024-02-25 19:24:58 -0300

    Enemy now will always spawn on land

--- SpawnController.gd
+++ SpawnController.gd
@@ -9,6 +9,7 @@ var elapsed := 0
 var player : CharacterBody2D
 var viewport : Vector2
 var rng : RandomNumberGenerator
+var map : TileMap
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -17,27 +18,35 @@ func _ready():
 	var camera = player.get_node("Camera2D")
 	viewport = camera.get_viewport_rect().size
 	rng = RandomNumberGenerator.new()
-
+	map = owner.get_node("Map")
 
 func _on_timeout() -> void:
 	elapsed += 1
 	for enemy in Enemies:
 		if elapsed % enemy.spawn_interval == 0:
-			var spawned := enemy.scene.instantiate()
-			spawned.position = _get_spawn_pos()
-			spawned.player = player
-			add_child(spawned)
+			var pos := _get_spawn_pos()
+			if pos:
+				var spawned := enemy.scene.instantiate()
+				spawned.position = pos
+				spawned.player = player
+				add_child(spawned)
 
 func _get_spawn_pos() -> Vector2:
-	var side := rng.randi_range(0, 3)
-	match side:
-		TOP:
-			return player.position + Vector2(randf_range(-0.5 * viewport.x, viewport.x / 2), -1 * viewport.y / 2.0)
-		DOWN:
-			return player.position + Vector2(randf_range(-0.5 * viewport.x, viewport.x / 2), viewport.y / 2.0)
-		LEFT:
-			return player.position + Vector2(viewport.x / 2.0, randf_range(-0.5 * viewport.x, -1 * viewport.x / 2))
-		RIGHT:
-			return player.position + Vector2(viewport.x / 2.0, randf_range(-0.5 * viewport.x, viewport.x / 2))
-	# TODO error here
-	return player.position
+	var pos : Vector2
+	for i in range(5):
+		var side := rng.randi_range(0, 3)
+		match side:
+			TOP:
+				pos = player.position + Vector2(randf_range(-0.5 * viewport.x, viewport.x / 2), -1 * viewport.y / 2.0)
+			DOWN:
+				pos = player.position + Vector2(randf_range(-0.5 * viewport.x, viewport.x / 2), viewport.y / 2.0)
+			LEFT:
+				pos = player.position + Vector2(viewport.x / 2.0, randf_range(-0.5 * viewport.x, -1 * viewport.x / 2))
+			RIGHT:
+				pos = player.position + Vector2(viewport.x / 2.0, randf_range(-0.5 * viewport.x, viewport.x / 2))
+		pos = pos / 16
+		print(map.get_cell_source_id(1, Vector2i(pos)))
+		if (map.get_cell_source_id(1, Vector2i(pos)) == 0):
+			return pos
+	
+	return Vector2.ZERO
